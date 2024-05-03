479ef9b4e3a74f911e55d12006963ae0977c692a
CodingManoj <CodingManoj@gmail.com>
2024-05-02 20:57:35 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-module "alb" {
+module "alb-public" {
     source                  = "./vendor/modules/alb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
     ENV                     = var.ENV
 }
 
-# We cannot parametrize anything that's added n the source.
-# To limit that, we can use a tool called as Terrafile. All it does is clonnes the remote code from the specified branch and make it locally available.
-
-
+module "alb-private" {
+    source                  = "./vendor/modules/alb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
+    ENV                     = var.ENV
+}
 
