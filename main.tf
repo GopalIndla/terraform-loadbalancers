8c3ec28da48c77d608d69f7f6059591e18c4078d
CodingManoj <CodingManoj@gmail.com>
2024-05-02 21:26:25 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -2,11 +2,13 @@ module "alb-public" {
     source                  = "./vendor/modules/alb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
     ENV                     = var.ENV
     ALB_NAME                = "roboshop-${var.ENV}-public"
+    INTERNAL                = false 
 }
 
 module "alb-private" {
     source                  = "./vendor/modules/alb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
     ENV                     = var.ENV
     ALB_NAME                = "roboshop-${var.ENV}-private"
+    INTERNAL                = true 
 }
 
