94b3e3ea7ed5bd6593b6886df587a3b4bfceeded
CodingManoj <CodingManoj@gmail.com>
2024-05-02 20:55:57 -0400

    Auto Push

--- main.tf
+++ main.tf
@@ -1,17 +1,6 @@
 module "alb" {
     source                  = "./vendor/modules/alb/"       # Terrafile clones the backend module mentioned in the Terrafile and keeps it local to the root 
-    VPC_CIDR                = var.VPC_CIDR
-    PUBLIC_SUBNET_CIDR      = var.PUBLIC_SUBNET_CIDR
-    PRIVATE_SUBNET_CIDR     = var.PRIVATE_SUBNET_CIDR
     ENV                     = var.ENV
-    AZ                      = var.AZ
-    DEFAULT_VPC_ID          = var.DEFAULT_VPC_ID
-    DEFAULT_VPC_CIDR        = var.DEFAULT_VPC_CIDR
-    DEFAULT_VPC_RT          = var.DEFAULT_VPC_RT
-    PUBLIC_HOSTEDZONE_ID    = var.PUBLIC_HOSTEDZONE_ID
-    PUBLIC_HOSTEDZONE_NAME  = var.PUBLIC_HOSTEDZONE_NAME
-    PRIVATE_HOSTEDZONE_ID   = var.PRIVATE_HOSTEDZONE_ID
-    PRIVATE_HOSTEDZONE_NAME = var.PRIVATE_HOSTEDZONE_NAME
 }
 
 # We cannot parametrize anything that's added n the source.
--- vars.tf
+++ vars.tf
@@ -1,17 +1,19 @@
-variable "VPC_CIDR" {}
-variable "PUBLIC_SUBNET_CIDR" {}
-variable "PRIVATE_SUBNET_CIDR" {}
-
-variable "ENV" {}
 variable "AZ" {}
 
-variable "DEFAULT_VPC_ID" {}
-variable "DEFAULT_VPC_CIDR" {}
-variable "DEFAULT_VPC_RT" {}
+# variable "VPC_CIDR" {}
+# variable "PUBLIC_SUBNET_CIDR" {}
+# variable "PRIVATE_SUBNET_CIDR" {}
 
-# Hosted  Zone Variables 
-variable "PUBLIC_HOSTEDZONE_ID" {}
-variable "PUBLIC_HOSTEDZONE_NAME" {}
+# variable "ENV" {}
+# variable "AZ" {}
 
-variable "PRIVATE_HOSTEDZONE_ID" {}
-variable "PRIVATE_HOSTEDZONE_NAME" {}
+# variable "DEFAULT_VPC_ID" {}
+# variable "DEFAULT_VPC_CIDR" {}
+# variable "DEFAULT_VPC_RT" {}
+
+# # Hosted  Zone Variables 
+# variable "PUBLIC_HOSTEDZONE_ID" {}
+# variable "PUBLIC_HOSTEDZONE_NAME" {}
+
+# variable "PRIVATE_HOSTEDZONE_ID" {}
+# variable "PRIVATE_HOSTEDZONE_NAME" {}
