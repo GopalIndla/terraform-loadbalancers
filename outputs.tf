07ddeb5f3a8b8afd31a7a0159242f8805b014770
CodingManoj <CodingManoj@gmail.com>
2024-05-06 21:45:27 -0400

    Auto Push

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
+output "PUBLIC_ALB_ARN" {
+  value = module.alb-public.ALB_ARN
+}
+
+output "PRIVATE_ALB_ARN" {
+  value = module.alb-private.ALB_ARN
+}
+
+output "PUBLIC_ALB_ADDRESS" {
+  value = module.alb-public.ALB_ADDRESS
+}
+
+output "PRIVATE_ALB_ADDRESS" {
+  value = module.alb-private.ALB_ADDRESS
+}
+
+
 # output "VPC_ID" {
 #     value = module.vpc.VPC_ID 
 # }
